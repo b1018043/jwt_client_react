a9ee4cec2035976eb3910419c6630c0c5ff844f2
unknown <grey.fox.0417@gmail.com>
2019-08-25 14:54:30 +0900

    add:patch and delete method

--- src/apis/Todo.js
+++ src/apis/Todo.js
@@ -38,3 +38,43 @@ export function postTodoData(token,todo){
             return {err};
         })
 }
+
+export function patchTodoData(token,todoid,process){
+    return request.patch(`http://localhost:8080/private`)
+        .set("Content-Type", "application/json")
+        .set("Authorization", `Bearer ${token}`)
+        .send({todoid:todoid,process:process})
+        .then(res=>{
+            return JSON.parse(res.text);
+        })
+        .catch(err=>{
+            if(err.status===400){
+                console.log("無効なリクエストです")
+            }else if(err.status===500){
+                console.log("もう一回")
+            }else{
+                console.log("無理")
+            }
+            return {err};
+        })
+}
+
+export function deleteTodoData(token,todoid){
+    return request.delete(`http://localhost:8080/private`)
+        .set("Content-Type", "application/json")
+        .set("Authorization", `Bearer ${token}`)
+        .send({todoid:todoid})
+        .then(res=>{
+            return JSON.parse(res.text);
+        })
+        .catch(err=>{
+            if(err.status===400){
+                console.log("無効なリクエストです")
+            }else if(err.status===500){
+                console.log("もう一回")
+            }else{
+                console.log("無理")
+            }
+            return {err};
+        })
+}
