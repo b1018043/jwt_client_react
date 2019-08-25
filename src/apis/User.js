718ed040c6985290e71d54d934b4d308b5d19140
unknown <grey.fox.0417@gmail.com>
2019-08-25 12:29:32 +0900

    add : add sendSignUpRequest

--- src/apis/User.js
+++ src/apis/User.js
@@ -23,3 +23,24 @@ export function sendLoginRequest(email, pass) {
             return { err };
         })
 }
+
+export function sendSignUpRequest(username,email,pass){
+    return request.post(`http://localhost:8080/login`)
+        .set("Content-Type", "application/json")
+        .send({username:username,email:email,password:pass})
+        .then(res=>{
+            const json=JSON.parse(res.text);
+            const {token}=json;
+            return token;
+        })
+        .catch(err=>{
+            if(err.status===400){
+                console.log("無効なリクエストです")
+            }else if(err.status===417){
+                console.log("もう一回送りなおしてください")
+            }else{
+                console.log("無理")
+            }
+            return {err};
+        })
+}
