4f5440fb0f23507f15e7b213804cec3226b1e7c8
unknown <grey.fox.0417@gmail.com>
2019-08-26 14:56:19 +0900

    add : action about signup

--- src/actions/User.js
+++ src/actions/User.js
@@ -2,6 +2,9 @@ export const LOGIN_REQUEST ="LOGIN_REQUEST";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAILED = "LOGIN_FAILED";
 export const LOGOUT ="LOGOUT";
+export const SIGNUP_REQUEST ="SIGNUP_REQUEST";
+export const SIGNUP_SUCCESS ="SIGNUP_SUCCESS";
+export const SIGNUP_FAILED ="SIGNUP_FAILED";
 
 export const loginRequest=(email,pass)=>({
     type: LOGIN_REQUEST,
@@ -20,4 +23,19 @@ export const loginFailed=(err)=>({
 
 export const logout=()=>({
     type:LOGOUT
+})
+
+export const signUpRequest = (username,email,pass)=>({
+    type: SIGNUP_REQUEST,
+    payload: {username,email,pass}
+})
+
+export const signUpSuccess=(token)=>({
+    type: SIGNUP_SUCCESS,
+    payload: token
+})
+
+export const signUpFailed=(err)=>({
+    type: SIGNUP_FAILED,
+    payload: err
 })
