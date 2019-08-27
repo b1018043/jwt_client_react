3c74e0d5d43a56eba1c4c2ba9fd3b7ca0d40c5e5
unknown <grey.fox.0417@gmail.com>
2019-08-27 14:29:48 +0900

    change: rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,12 @@
 import {combineReducers} from "redux";
 import {connectRouter} from "connected-react-router";
+import userReducer from "./User";
+import todoReducer from "./Todo";
 
 const rootReducer=history=>combineReducers({
     router: connectRouter(history),
+    user: userReducer,
+    todo: todoReducer,
 });
 
 export default rootReducer;
