5da7a7056e0ccaeaf596da8fb1fe3203ecfe0c7c
unknown <grey.fox.0417@gmail.com>
2019-08-29 15:09:01 +0900

    add:エラーページに5秒以上いるとほかのページに飛ぶようにした

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,9 +1,34 @@
 import React from "react";
+import {useSelector} from "react-redux";
+import {Typography} from "@material-ui/core";
+import {Redirect} from "react-router-dom";
 
 function Error(){
+    const {token}=useSelector(state=>state.user);
+    const [count,setCount]=React.useState(0);
+    React.useEffect(()=>{
+        function countUp() {
+            setCount(count + 1);
+        }
+        const id=setInterval(()=>{
+            countUp();
+        },1000);
+        return ()=>{
+            clearInterval(id);
+        }
+    })
+    function moveAt(token){
+        return token ? <Redirect to="/todo" /> : <Redirect to="/login" />;
+    }
     return (
         <div>
-            Error 404 not found.
+            <Typography>
+                Error 404 not found.
+            </Typography>
+            <Typography>
+                {count>5?0:5-count} seconds later move page.
+            </Typography>
+            {(count>5)&&moveAt(token)}
         </div>
     )
 }
