7993fc8e06a8628bcd797ec03f346eea341ad0e4
unknown <grey.fox.0417@gmail.com>
2019-08-29 15:41:06 +0900

    add:add todo

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Botton from "@material-ui/core/Button";
-import {useDispatch} from "react-redux";
+import {useDispatch,useSelector} from "react-redux";
 import {logout} from "../actions/User";
+import {postTodoRequest} from "../actions/Todo";
 
-import {makeStyles} from "@material-ui/core"
+import {makeStyles, TextField} from "@material-ui/core"
 
 const useStyles=makeStyles({
     bar:{
@@ -15,6 +16,8 @@ const useStyles=makeStyles({
 function MyAppBar(){
     const classes=useStyles()
     const dispatch=useDispatch();
+    const {token}=useSelector(state=>state.user);
+    const [todo,setTodo]=React.useState("");
     return (
         <AppBar className={classes.bar} position="relative">
             <Botton
@@ -23,6 +26,21 @@ function MyAppBar(){
                 }}
             >
                 Logout
+            </Botton>
+            <TextField
+                label="Todo"
+                value={todo}
+                onChange={(e)=>{
+                    setTodo(e.target.value)
+                }}
+            />
+            <Botton
+                onClick={()=>{
+                    dispatch(postTodoRequest(token,todo))
+                    setTodo("")
+                }}
+            >
+                Add Todo
             </Botton>
         </AppBar>
     )
